ab427893c55fbd28453a40af1c7072d09a741890
Bardh Kastrati <bk44536@ubt-uni.net>
2020-05-01 23:27:07 +0200

    last commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var x = document.getElementById("login")
+var y = document.getElementById("register")
+
+function register(){
+    x.style.left = "-420px"
+    y.style.left = "78px"
+}
+
+function login(){
+    x.style.left = "0px"
+    y.style.left = "498px"
+}
